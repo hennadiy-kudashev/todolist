9a3267f5b8ea47e8881921d7603963b164223d7c
utyf <gena.utyf@gmail.com>
2015-07-02 16:55:26 +0300

    Moved index.html to public

--- rest/approute.js
+++ rest/approute.js
@@ -4,11 +4,16 @@ var router = express.Router();
 var ItemRepository = require('./repository/itemRepository');
 var itemRepository = new ItemRepository();
 
-exports.register = function(app) {
-	router.route('/todo')
-		.get(function(request, response) {
-			response.sendfile('index.html');
-		});
-	app.use('/api', router);
-	app.use('/api', express.static('public'));
+exports.register = function (app) {
+    router.route('/todo')
+        .get(function (request, response) {
+            var items = itemRepository.getAll();
+            response.json(items);
+        });
+    app.use('/api', router);
+
+    app.use(express.static('public'));
+    app.get('/', function (request, response) {
+        response.sendfile('public/index.html');
+    });
 };
